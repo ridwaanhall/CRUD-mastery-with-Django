fa7b034ff4741c682e06c53c880a36c274b856b4
ridwaanhall <erbyl14@gmail.com>
2023-11-08 14:59:28 +0700

    create - CRUD

--- crm/webapp/forms.py
+++ crm/webapp/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import Record
 from django import forms
 
 from django.contrib.auth.forms import AuthenticationForm
@@ -17,3 +18,15 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+    
+# create a record
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
+        
+# Update a record
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model = Record
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'province', 'country']
--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.home, name=""),
     path('register', views.register, name="register"),
     path('my-login', views.my_login, name="my-login"),
-    path('dashboard', views.dashboard, name="dashboard"),
     path('user-logout', views.user_logout, name="user-logout"),
+    
+    # CRUD
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('create-record', views.create_record, name="create-record"),
 ]
